e18efc7e0ebc5d704d4e27f0201979514e9e6103
Annika <annika.bhatia@gmail.com>
2025-06-23 17:40:09 -0400

    updated code changes

--- easy_problems/length_of_last_word.py
+++ easy_problems/length_of_last_word.py
@@ -3,12 +3,14 @@
 #this code doesnt pass case two, so still need to tweak
 def lengthOfLastWord(s: str) -> int:
         length = 0
-        for char in s:
+        trimmed_string = s.rstrip()
+        for char in trimmed_string:
             if char == ' ':
-                length = 0
+                length=0
+                continue
             else:
                 length+=1
         return length
 
-s = "Hello World"
+s = "   fly me   to   the moon  "
 print(lengthOfLastWord(s))
